c9ac325fda0e82fc527a57cbb520594d35b6b8f4
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-19 11:17:55 +0400

    Print account data

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ account_a = random.choice(data)
 account_b = random.choice(data)
 if account_a == account_b:
     account_b = random.choice(data)
+
+#Format the account data into printable format
+account_name = account_a["name"]
+account_descr = account_a["description"]
+account_country = account_a["country"]
+print(f"{account_name}, a {account_descr}, from {account_country}")
+
