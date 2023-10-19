8cf5c4005b6734e828782c3d6f35d182184f5965
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-19 11:23:57 +0400

    Writing A and B variants with funksiya

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from art import logo
 from game_data import data
 import random
 
+
+#Format the account data into printable format
+def format_data(account):
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
+
 print(logo)
 #Generate a random account from the game data
 account_a = random.choice(data)
@@ -9,9 +18,8 @@ account_b = random.choice(data)
 if account_a == account_b:
     account_b = random.choice(data)
 
-#Format the account data into printable format
-account_name = account_a["name"]
-account_descr = account_a["description"]
-account_country = account_a["country"]
-print(f"{account_name}, a {account_descr}, from {account_country}")
+print(f"Compare A: {format_data(account_a)}")
+print(f"Compare B: {format_data(account_b)}")
+
+
 
