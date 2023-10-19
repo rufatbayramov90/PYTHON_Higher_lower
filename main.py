08a3c3744e338b4dd76a02067f91763d9c9d9cb3
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-19 12:40:11 +0400

    Takes the user guess and follower counts and return if they got it right

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from art import logo
+from art import logo, vs
 from game_data import data
 import random
 
 
-#Format the account data into printable format
+#Takes the account data and return the printable format
 def format_data(account):
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
 
+#Takes the user guess and follower counts and return if they got it right
+def check_answer(guess, a_folowers, b_followers):
+    if a_folowers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+     
 
 print(logo)
 #Generate a random account from the game data
@@ -19,7 +27,19 @@ if account_a == account_b:
     account_b = random.choice(data)
 
 print(f"Compare A: {format_data(account_a)}")
+print(vs)
 print(f"Compare B: {format_data(account_b)}")
+
+#Ask user for a guess
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+#Get follower count of each account   
+a_follower_count = account_a["follower_count"]
+b_follower_count = account_b["follower_count"]
+
+is_correct = check_answer(guess,a_follower_count,b_follower_count )
+
+
 
 
 
