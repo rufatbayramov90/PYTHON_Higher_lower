1f3b7ec67726cf3239d27fb235e168d2abf52fbd
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-20 10:30:44 +0400

    clear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from art import logo, vs
 from game_data import data
 import random
+from replit import clear
 
 
 #Takes the account data and return the printable format
@@ -22,11 +23,13 @@ def check_answer(guess, a_folowers, b_followers):
 print(logo)
 score = 0
 game_should_continue =True
+ #Generate a random account from the game data
+account_b = random.choice(data)
 
 #Make the game repeatable
 while game_should_continue:        
-    #Generate a random account from the game data
-    account_a = random.choice(data)
+    #Make account at position B become the next account at position A
+    account_a = account_b
     account_b = random.choice(data)
     if account_a == account_b:
         account_b = random.choice(data)
@@ -42,7 +45,9 @@ while game_should_continue:
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
     is_correct = check_answer(guess,a_follower_count,b_follower_count )
-
+    #clear the screen between rounds
+    clear() 
+    print(logo)
     #Give user feedback on their guess
     if is_correct:
         score += 1
