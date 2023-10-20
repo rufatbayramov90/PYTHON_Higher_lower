4bb2aec4ee48b8e37bc72021bb2d3bfc6fe21d70
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-20 10:17:00 +0400

    Playing the game over and over again

--- main.py
+++ main.py
@@ -20,24 +20,36 @@ def check_answer(guess, a_folowers, b_followers):
      
 
 print(logo)
-#Generate a random account from the game data
-account_a = random.choice(data)
-account_b = random.choice(data)
-if account_a == account_b:
+score = 0
+game_should_continue =True
+
+#Make the game repeatable
+while game_should_continue:        
+    #Generate a random account from the game data
+    account_a = random.choice(data)
     account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}")
-print(vs)
-print(f"Compare B: {format_data(account_b)}")
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}")
 
-#Ask user for a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    #Ask user for a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-#Get follower count of each account   
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    #Get follower count of each account   
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess,a_follower_count,b_follower_count )
 
-is_correct = check_answer(guess,a_follower_count,b_follower_count )
+    #Give user feedback on their guess
+    if is_correct:
+        score += 1
+        print(f"You're right {score}")
+    else:
+        game_should_continue = False
+        print(f"Dorry, that's wrong {score}")
 
 
 
